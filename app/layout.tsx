1eeed0ef01725f9e3b46ee0862b6c3fef110d6de
artur <159655857+ArturYakubenko@users.noreply.github.com>
2026-01-04 19:20:35 +0100

    xcvxvcfgfgdfgdf

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Header from "@/components/Header/Header";
-import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
+import TanStackProvider from "@/components/TanStackProvider/huita";
 import Footer from "@/components/Footer/Footer";
 
 ////
